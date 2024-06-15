6453831f219ecaa6275673af1335250da07e1927
bryanfelipe154 <89936744+bryanfelipe154@users.noreply.github.com>
2024-06-15 13:19:47 -0300

    Update Render.cpp

--- DirectX App/Render.cpp
+++ DirectX App/Render.cpp
@@ -18,11 +18,8 @@ struct RDTLVERTEX
 
 void RenderRectProgress(float fProgress, LPDIRECT3DDEVICE9 renderDevice)
 {
-	if (fProgress < 1.0f)
+	if (fProgress > 0.0f)
 	{
-		//fProgress = 1.0f - fProgress; // todo: inverte o processo, funciona do jeito que tem que ser, criando o retangulo preenchido e desfazendo ele
-		// porem esta no sentido anti horario, isso tem que ser feito no sentido horario
-
 		renderDevice->SetFVF(324);
 
 		float iX = 50.0f;
@@ -31,66 +28,47 @@ void RenderRectProgress(float fProgress, LPDIRECT3DDEVICE9 renderDevice)
 		float iCX = 100.0f;
 		float iCY = 100.0f;
 
-		DWORD dwColor = 0xFF000000 | 0xFF1493;
-
 		float fSX = (float)(iCX / 2.0f) + iX;
 		float fSY = (float)(iCY / 2.0f) + iY;
 		float fWidth = iCX / 2.0f;
 		float fHeight = iCY / 2.0f;
 
-		RDTLVERTEX vertex[10];
+		DWORD dwColor = 0xFF000000 | 0xFF1493;
 
-		std::fill(std::begin(vertex), std::end(vertex), RDTLVERTEX{ .diffuse = dwColor });
+		std::vector<RDTLVERTEX> vertex =
+		{
+			{ { fSX, fSY }, 0.0f, dwColor },
+			{ { fSX, fSY - fHeight }, 0.0f, dwColor },
+			{ { fSX + fWidth, fSY - fHeight }, 0.0f, dwColor },
+			{ { fSX + fWidth, fSY }, 0.0f, dwColor },
+			{ { fSX + fWidth, fSY + fHeight }, 0.0f, dwColor },
+			{ { fSX, fSY + fHeight }, 0.0f, dwColor },
+			{ { fSX - fWidth, fSY + fHeight }, 0.0f, dwColor },
+			{ { fSX - fWidth, fSY }, 0.0f, dwColor },
+			{ { fSX - fWidth, fSY - fHeight }, 0.0f, dwColor },
+			{ { fSX, fSY - fHeight }, 0.0f, dwColor }
+		};
 
-		vertex[0].position = { fSX, fSY };
-		vertex[1].position = { fSX, fSY - fHeight };
-		vertex[2].position = { fSX + fWidth, fSY - fHeight };
-		vertex[3].position = { fSX + fWidth, fSY };
-		vertex[4].position = { fSX + fWidth, fSY + fHeight };
-		vertex[5].position = { fSX, fSY + fHeight };
-		vertex[6].position = { fSX - fWidth, fSY + fHeight };
-		vertex[7].position = { fSX - fWidth, fSY };
-		vertex[8].position = { fSX - fWidth, fSY - fHeight };
+		std::map<float, TMVector3> mapProgress =
+		{
+			{ 0.125f, { fSX + fWidth * (fProgress / 0.125f), fSY - fHeight} },
+			{ 0.25f, { fSX + fWidth, fSY - fHeight + fHeight * ((fProgress - 0.125f) / 0.125f) } },
+			{ 0.375f, { fSX + fWidth, fSY + fHeight * ((fProgress - 0.25f) / 0.125f) } },
+			{ 0.5f, { fSX + fWidth - fWidth * ((fProgress - 0.375f) / 0.125f), fSY + fHeight} },//
+			{ 0.625f, { fSX - fWidth * ((fProgress - 0.5f) / 0.125f), fSY + fHeight } },
+			{ 0.75f, { fSX - fWidth, fSY + fHeight - fHeight * ((fProgress - 0.625f) / 0.125f) } },
+			{ 0.875f, { fSX - fWidth, fSY - fHeight * ((fProgress - 0.75f) / 0.125f) } },
+			{ 1.0f, { fSX - fWidth + fWidth * ((fProgress - 0.875f) / 0.125f), fSY - fHeight } }
+		};
 
-		if (fProgress <= 0.125f)
+		for (const auto&& [i, it] : std::ranges::enumerate_view(mapProgress))
 		{
-			vertex[2].position = { fSX + fWidth * (fProgress / 0.125f), fSY - fHeight };
-			renderDevice->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 1u, vertex, sizeof(RDTLVERTEX));
-		}
-		else if (fProgress <= 0.25f)
-		{
-			vertex[3].position = { fSX + fWidth, fSY - fHeight + fHeight * ((fProgress - 0.125f) / 0.125f) };
-			renderDevice->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2u, vertex, sizeof(RDTLVERTEX));
-		}
-		else if (fProgress <= 0.375f)
-		{
-			vertex[4].position = { fSX + fWidth, fSY + fHeight * ((fProgress - 0.25f) / 0.125f) };
-			renderDevice->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 3u, vertex, sizeof(RDTLVERTEX));
-		}
-		else if (fProgress <= 0.5f)
-		{
-			vertex[5].position = { fSX + fWidth - fWidth * ((fProgress - 0.375f) / 0.125f), fSY + fHeight };
-			renderDevice->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 4u, vertex, sizeof(RDTLVERTEX));
-		}
-		else if (fProgress <= 0.625f)
-		{
-			vertex[6].position = { fSX - fWidth * ((fProgress - 0.5f) / 0.125f), fSY + fHeight };
-			renderDevice->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 5u, vertex, sizeof(RDTLVERTEX));
-		}
-		else if (fProgress <= 0.75f)
-		{
-			vertex[7].position = { fSX - fWidth, fSY + fHeight - fHeight * ((fProgress - 0.625f) / 0.125f) };
-			renderDevice->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 6u, vertex, sizeof(RDTLVERTEX));
-		}
-		else if (fProgress <= 0.875f)
-		{
-			vertex[8].position = { fSX - fWidth, fSY - fHeight * ((fProgress - 0.75f) / 0.125f) };
-			renderDevice->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 7u, vertex, sizeof(RDTLVERTEX));
-		}
-		else if (fProgress <= 1.0f)
-		{
-			vertex[9].position = { fSX - fWidth + fWidth * ((fProgress - 0.875f) / 0.125f), fSY - fHeight };
-			renderDevice->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 8u, vertex, sizeof(RDTLVERTEX));
+			if (fProgress <= it.first)
+			{
+				vertex[i + 2].position = it.second;
+				renderDevice->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, i + 1, vertex.data(), sizeof(RDTLVERTEX));
+				break;
+			}
 		}
 	}
 }
